592a37540082fbab10d3d48b246927784b54aba3
Mohamed-Mosad1 <mohamedmosaad115@gmail.com>
2023-10-16 03:51:34 +0300

    upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './core/guard/auth.guard';
+
+const routes: Routes = [
+  // blank layout
+  { path: '', canActivate: [authGuard], loadComponent:()=>import('./layouts/blank-layout/blank-layout.component').then((m)=>m.BlankLayoutComponent) ,
+  children: [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', loadComponent:()=>import('./components/home/home.component').then((m)=>m.HomeComponent), title:"Home" },
+    { path: 'cart', loadComponent:()=>import('./components/cart/cart.component').then((m)=>m.CartComponent), title:"Cart" },
+    { path: 'products', loadComponent:()=>import('./components/products/products.component').then((m)=>m.ProductsComponent), title:"Products" },
+    { path: 'details/:id', loadComponent:()=>import('./components/details/details.component').then((m)=>m.DetailsComponent), title:"Products" },
+    { path: 'brands', loadComponent:()=>import('./components/brands/brands.component').then((m)=>m.BrandsComponent), title:"Brands" },
+    { path: 'brandsDetails/:id', loadComponent:()=>import('./components/brands-details/brands-details.component').then((m)=>m.BrandsDetailsComponent), title:"Brands Details" },
+    { path: 'categories', loadComponent:()=>import('./components/categories/categories.component').then((m)=>m.CategoriesComponent), title:"Categories" },
+    { path: 'categoriesDetails/:id', loadComponent:()=>import('./components/categories-details/categories-details.component').then((m)=>m.CategoriesDetailsComponent), title:"Categories Details" },
+    { path: 'payment/:id', loadComponent:()=>import('./components/payment/payment.component').then((m)=>m.PaymentComponent), title:"Payment" },
+    { path: 'wishlist', loadComponent:()=>import('./components/wishlist/wishlist.component').then((m)=>m.WishlistComponent), title:"Wish List" },
+    { path: 'allorders', loadComponent:()=>import('./components/allorders/allorders.component').then((m)=>m.AllordersComponent), title:"All Orders" },
+
+  ]},
+  // auth layout
+  {path: '',loadComponent:()=>import('./layouts/auth-layout/auth-layout.component').then((m)=>m.AuthLayoutComponent) ,
+  children:[
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', loadComponent:()=>import('./components/login/login.component').then((m)=>m.LoginComponent), title:"Login" },
+    { path: 'register', loadComponent:()=>import('./components/register/register.component').then((m)=>m.RegisterComponent), title:"Register" },
+  ]},
+  // not found
+  {path: '**',loadComponent:()=>import('./components/not-found-page/not-found-page.component').then((m)=>m.NotFoundPageComponent) , title:"Page Not Found"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesInterface, Product } from 'src/app/core/interfaces/product';
+import { CategoriesService } from 'src/app/core/services/categories.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss']
+})
+export class CategoriesComponent implements OnInit {
+
+  constructor(private _CategoriesService:CategoriesService) { }
+
+  categoriesData: CategoriesInterface[] = [];
+
+
+  ngOnInit(): void {
+    this._CategoriesService.getCategories().subscribe({
+      next: (res) => {
+        this.categoriesData = res.data;
+      },
+    })
+  }
+
+
+}
